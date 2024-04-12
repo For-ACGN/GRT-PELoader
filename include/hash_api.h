108dd146ef544a452ac8dc1ea4536c477c6f9c9e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-12 20:38:12 +0800

    add project files about hash api, improve project structure.

---
+++ include/hash_api.h
@@ -0,0 +1,12 @@
+#ifndef HASH_API_H
+#define HASH_API_H
+
+#include "go_types.h"
+
+#ifdef _WIN64
+	uintptr FindAPI(uint64 hash, uint64 key);
+#elif _WIN32
+	uintptr FindAPI(uint32 hash, uint32 key);
+#endif
+
+#endif // HASH_API_H
