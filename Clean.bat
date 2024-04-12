a01a45e9c2bdd4546d04702da63491565023c220
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-12 20:34:14 +0800

    add a clean script for clean Debug and Release output files.

---
+++ Clean.bat
@@ -0,0 +1,12 @@
+del /S /Q ".vs\PE-Shelter\v17\ipch\AutoPCH"
+del /S /Q ".vs\PE-Shelter\v17\Browse.VC.db"
+
+rd /S /Q "Debug"
+rd /S /Q "Release"
+rd /S /Q "x86"
+rd /S /Q "x64"
+
+rd /S /Q "example\win_exec\x86"
+rd /S /Q "example\win_exec\x64"
+rd /S /Q "example\win_exec\Debug"
+rd /S /Q "example\win_exec\Release"
