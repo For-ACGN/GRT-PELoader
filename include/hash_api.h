095162388a549a1c76ae3e706cb89a61478c0252
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-13 15:35:44 +0800

    hash_api.h: add comment for FindAPI, add typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key).

--- include/hash_api.h
+++ include/hash_api.h
@@ -4,9 +4,19 @@
 #include "go_types.h"
 
 #ifdef _WIN64
-	uintptr FindAPI(uint64 hash, uint64 key);
+
+typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key);
+
+// FindAPI is used to FindAPI address by hash and key.
+uintptr FindAPI(uint64 hash, uint64 key);
+
 #elif _WIN32
-	uintptr FindAPI(uint32 hash, uint32 key);
+
+typedef uintptr (*FindAPI_t)(uint32 hash, uint32 key);
+
+// FindAPI is used to FindAPI address by hash and key.
+uintptr FindAPI(uint32 hash, uint32 key);
+
 #endif
 
 #endif // HASH_API_H
