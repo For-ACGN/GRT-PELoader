607c0b66f2cf0da789f675be386c081c84d25d52
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-14 16:25:40 +0800

    loader/loader.c: add read a test PE file for PELoader.

--- example/loader/loader.c
+++ example/loader/loader.c
@@ -1,8 +1,40 @@
+#include <stdio.h>
+#include <windows.h>
+
 #include "go_types.h"
+#include "hash_api.h"
 #include "pe_shelter.h"
 
 #pragma comment(linker, "/ENTRY:EntryMain")
-uint EntryMain() {
+uint EntryMain() 
+{
+    HANDLE hFile = CreateFileA("D:\\hash.exe", GENERIC_READ, 0, NULL, OPEN_ALWAYS, 0, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return -1;
+    }
+    DWORD dwSize;
+    dwSize = GetFileSize(hFile, 0);
 
-    return LoadPE(0);
+    LPVOID lpAddress = VirtualAlloc(NULL, dwSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    if (lpAddress == NULL)
+    {
+        CloseHandle(hFile);
+        return -1;
+    }
+    DWORD dwRead;
+    if (!ReadFile(hFile, lpAddress, dwSize, &dwRead, 0))
+    {
+        CloseHandle(hFile);
+        return -1;
+    }
+
+
+    PEShelterCtx context = {
+        .FindAPI  = FindAPI,
+        .Hooks    = NULL,
+        .NumHooks = 1,
+    };
+    uintptr entry = LoadPE(&context, lpAddress, dwSize);
+    return entry;
 }
