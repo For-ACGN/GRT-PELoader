325594ef62da1db85b4be7e4525e3e2ff23f2810
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-14 16:44:22 +0800

    pe_shelter.h: add typedef FindAPI_t, remove typedef in hash_api.h.

--- include/hash_api.h
+++ include/hash_api.h
@@ -3,20 +3,11 @@
 
 #include "go_types.h"
 
+// FindAPI is used to FindAPI address by hash and key.
 #ifdef _WIN64
-
-typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key);
-
-// FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint64 hash, uint64 key);
-
 #elif _WIN32
-
-typedef uintptr (*FindAPI_t)(uint32 hash, uint32 key);
-
-// FindAPI is used to FindAPI address by hash and key.
 uintptr FindAPI(uint32 hash, uint32 key);
-
 #endif
 
 #endif // HASH_API_H
--- include/pe_shelter.h
+++ include/pe_shelter.h
@@ -2,7 +2,12 @@
 #define PE_SHELTER_H
 
 #include "go_types.h"
-#include "hash_api.h"
+
+#ifdef _WIN64
+typedef uintptr (*FindAPI_t)(uint64 hash, uint64 key);
+#elif _WIN32
+typedef uintptr (*FindAPI_t)(uint32 hash, uint32 key);
+#endif
 
 typedef struct {
     uintptr Original;
