7f52f7d83ebf6127e31215544d785b0c99e6d676
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-14 16:26:31 +0800

    pe_shelter.h: add typedef struct Hook and PEShelterCtx.

--- include/pe_shelter.h
+++ include/pe_shelter.h
@@ -2,7 +2,19 @@
 #define PE_SHELTER_H
 
 #include "go_types.h"
+#include "hash_api.h"
 
-uintptr LoadPE(uintptr address);
+typedef struct {
+    uintptr Original;
+    uintptr Replace;
+} Hook;
+
+typedef struct {
+	FindAPI_t FindAPI;
+    Hook      (*Hooks)[];
+    uint      NumHooks;
+} PEShelterCtx;
+
+uintptr LoadPE(PEShelterCtx *ctx, uintptr address, uint64 size);
 
 #endif // PE_SHELTER_H
