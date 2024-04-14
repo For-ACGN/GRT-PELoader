047fa1d5baf437a8ef18295335d7c37cf72f79fe
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-14 16:27:10 +0800

    windows_c.h: rename header file and add function typedef.

---
+++ include/windows_c.h
@@ -0,0 +1,76 @@
+#ifndef WINDOWS_H
+#define WINDOWS_H
+
+#include "go_types.h"
+
+/* 
+* Documents:
+* https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualalloc
+* https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
+* https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfree
+* https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibrarya
+* https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary
+* https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress
+* https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-flushinstructioncache
+* https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createthread
+*/
+
+typedef byte*   LPCSTR;
+typedef uintptr HMODULE;
+typedef uint    HANDLE;
+
+#define MEM_COMMIT  0x00001000
+#define MEM_RESERVE 0x00002000
+#define MEM_RELEASE 0x00008000
+
+#define PAGE_NOACCESS          0x01
+#define PAGE_READONLY          0x02
+#define PAGE_READWRITE         0x04
+#define PAGE_WRITECOPY         0x08
+#define PAGE_EXECUTE           0x10
+#define PAGE_EXECUTE_READ      0x20
+#define PAGE_EXECUTE_READWRITE 0x40
+#define PAGE_EXECUTE_WRITECOPY 0x80
+
+typedef uintptr (*VirtualAlloc)
+(
+    uintptr lpAddress, uint dwSize, uint32 flAllocationType, uint32 flProtect
+);
+
+typedef bool (*VirtualProtect)
+(
+    uintptr lpAddress, uint dwSize, uint32 flNewProtect, uint32* lpflOldProtect
+);
+
+typedef bool (*VirtualFree)
+(
+    uintptr lpAddress, uint dwSize, uint32 dwFreeType
+);
+
+typedef HMODULE (*LoadLibraryA)
+(
+    LPCSTR lpLibFileName
+);
+
+typedef bool (*FreeLibrary)
+(
+    HMODULE hLibModule
+);
+
+typedef uintptr (*GetProcAddress)
+(
+    HMODULE hModule, LPCSTR lpProcName
+);
+
+typedef bool (*FlushInstCache)
+(
+    HANDLE hProcess, uintptr lpBaseAddress, uint dwSize
+);
+
+typedef HANDLE(*CreateThread)
+(
+    uintptr lpThreadAttributes, uint dwStackSize, uintptr lpStartAddress,
+    uintptr lpParameter, uint32 dwCreationFlags, uint32* lpThreadId
+);
+
+#endif // WINDOWS_H
