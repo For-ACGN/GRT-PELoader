6ce4133712866fc93b55ab1e8db8fbabaa50c694
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-15 15:45:23 +0800

    pe_shelter.c: refactor PE loader, replace copyImage to mapPESections.

--- src/pe_shelter.c
+++ src/pe_shelter.c
@@ -3,7 +3,10 @@
 #include "pe_shelter.h"
 
 typedef struct {
-    PEShelterCtx* ctx;
+    PEShelterCtx* context;
+
+    // Arguments
+    uintptr ImageAddr;
 
     // API address
     VirtualAlloc   VirtualAlloc;
@@ -17,23 +20,25 @@ typedef struct {
 
     // PE image information
     uintptr PEImage;
-    uintptr ImageBase;
     uint32  PEOffset;
+    uintptr ImageBase;
     uintptr EntryPointRVA;
 
     // DLL
 } PEShelterRT;
 
 static bool initAPI(PEShelterRT* runtime);
-static bool copyPEImage(PEShelterRT* runtime, uintptr address, uint64 size);
 static bool parsePEImage(PEShelterRT* runtime);
+static bool mapPESections(PEShelterRT* runtime);
 static void fixRelocationTable(PEShelterRT* runtime);
 static void copyMemory(uintptr dst, uintptr src, uint len);
 
-uintptr LoadPE(PEShelterCtx* ctx, uintptr address, uint64 size) 
+uintptr LoadPE(PEShelterCtx* context, uintptr address)
 {
     PEShelterRT runtime = {
-        .ctx = ctx,
+        .context   = context,
+        .ImageAddr = address,
+
         // ignore Visual Studio bug fix
         .VirtualAlloc   = (VirtualAlloc)1,
         .VirtualProtect = (VirtualProtect)1,
@@ -44,94 +49,92 @@ uintptr LoadPE(PEShelterCtx* ctx, uintptr address, uint64 size)
         .FlushInstCache = (FlushInstCache)1,
         .CreateThread   = (CreateThread)1,
     };
-
     if (!initAPI(&runtime))
     {
         return NULL;
     }
     for (;;)
     {
-        if (!copyPEImage(&runtime, address, size))
-        {
-            break;
-        }
         if (!parsePEImage(&runtime))
         {
             break;
         }
+        if (!mapPESections(&runtime))
+        {
+            break;
+        }
         fixRelocationTable(&runtime);
-
         break;
     }
   
-    if (runtime.PEImage != NULL)
-    {
-
-    }
-    else
-    {
-
-    }
+    // if (runtime.PEImage != NULL)
+    // {
+    // 
+    // }
+    // else
+    // {
+    // 
+    // }
     return runtime.PEOffset;
 }
 
 // initAPI is used to find API addresses for PE loader.
 static bool initAPI(PEShelterRT* runtime)
 {
-    PEShelterCtx* ctx = runtime->ctx;
+    FindAPI_t findAPI = runtime->context->FindAPI;
 
     uint64 hash = 0xB6A1D0D4A275D4B6;
     uint64 key  = 0x64CB4D66EC0BEFD9;
-    VirtualAlloc virtualAlloc = (VirtualAlloc)ctx->FindAPI(hash, key);
+    VirtualAlloc virtualAlloc = (VirtualAlloc)findAPI(hash, key);
     if (virtualAlloc == NULL)
     {
         return false;
     }
     hash = 0x8CDC3CBC1ABF3F5F;
     key  = 0xC3AEEDC9843D7B34;
-    VirtualProtect virtualProtect = (VirtualProtect)ctx->FindAPI(hash, key);
+    VirtualProtect virtualProtect = (VirtualProtect)findAPI(hash, key);
     if (virtualProtect == NULL)
     {
         return false;
     }
     hash = 0xB82F958E3932DE49;
     key  = 0x1CA95AA0C4E69F35;
-    VirtualFree virtualFree = (VirtualFree)ctx->FindAPI(hash, key);
+    VirtualFree virtualFree = (VirtualFree)findAPI(hash, key);
     if (virtualFree == NULL)
     {
         return false;
     }
     hash = 0xC0B89BE712EE4C18;
     key  = 0xF80CA8B02538CAC4;
-    LoadLibraryA loadLibraryA = (LoadLibraryA)ctx->FindAPI(hash, key);
+    LoadLibraryA loadLibraryA = (LoadLibraryA)findAPI(hash, key);
     if (loadLibraryA == NULL)
     {
         return false;
     }
     hash = 0xC22B47E9D652D287;
     key  = 0xA118770E82EB0797;
-    FreeLibrary freeLibrary = (FreeLibrary)ctx->FindAPI(hash, key);
+    FreeLibrary freeLibrary = (FreeLibrary)findAPI(hash, key);
     if (freeLibrary == NULL)
     {
         return false;
     }
     hash = 0xB1AE911EA1306CE1;
     key  = 0x39A9670E629C64EA;
-    GetProcAddress getProcAddress = (GetProcAddress)ctx->FindAPI(hash, key);
+    GetProcAddress getProcAddress = (GetProcAddress)findAPI(hash, key);
     if (getProcAddress == NULL)
     {
         return false;
     }
     hash = 0x8172B49F66E495BA;
     key  = 0x8F0D0796223B56C2;
-    FlushInstCache flushInstCache = (FlushInstCache)ctx->FindAPI(hash, key);
+    FlushInstCache flushInstCache = (FlushInstCache)findAPI(hash, key);
     if (flushInstCache == NULL)
     {
         return false;
     }
     hash = 0x134459F9F9668FC1;
     key  = 0xB2877C84F94DB5D8;
-    CreateThread createThread = (CreateThread)ctx->FindAPI(hash, key);
+    CreateThread createThread = (CreateThread)findAPI(hash, key);
     if (createThread == NULL)
     {
         return false;
@@ -147,19 +150,31 @@ static bool initAPI(PEShelterRT* runtime)
     return true;
 }
 
-static bool copyPEImage(PEShelterRT* runtime, uintptr address, uint64 size)
+static bool parsePEImage(PEShelterRT* runtime)
+{
+    uintptr imageAddr = runtime->ImageAddr;
+    uint32  peOffset = *(uint32*)(imageAddr + 60);
+    uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 48);
+    uintptr entryPointRVA = *(uint32*)(imageAddr + peOffset + 40);
+    runtime->PEOffset = peOffset;
+    runtime->ImageBase = imageBase;
+    runtime->EntryPointRVA = entryPointRVA;
+    return true;
+}
+
+static bool mapPESections(PEShelterRT* runtime)
 {
     // allocate memory for PE image
-    uintptr peImage = runtime->VirtualAlloc(0, size, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+    uintptr peImage = runtime->VirtualAlloc(0, 123, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
     if (peImage == NULL)
     {
         return false;
     }
     // copy PE image to the memory
-    copyMemory(peImage, address, size);
+    // copyMemory(peImage, address, size);
     // change memory protect to executable
     uint32 oldProtect;
-    if (!runtime->VirtualProtect(peImage, size, PAGE_EXECUTE_READ, &oldProtect))
+    if (!runtime->VirtualProtect(peImage, 123, PAGE_EXECUTE_READ, &oldProtect))
     {
         return false;
     }
@@ -167,17 +182,7 @@ static bool copyPEImage(PEShelterRT* runtime, uintptr address, uint64 size)
     return true;
 }
 
-static bool parsePEImage(PEShelterRT* runtime)
-{
-    uintptr peImage = runtime->PEImage;
-    uint32  peOffset = *(uint32*)(peImage + 60);
-    uintptr imageBase = *(uintptr*)(peImage + peOffset + 48);
-    uintptr entryPointRVA = *(uint32*)(peImage + peOffset + 40);
-    runtime->PEOffset = peOffset;
-    runtime->ImageBase = imageBase;
-    runtime->EntryPointRVA = entryPointRVA;
-    return true;
-}
+
 
 static void fixRelocationTable(PEShelterRT* runtime)
 {
