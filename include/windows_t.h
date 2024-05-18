e1692c7d67465f1d0f88790dbb619be31e1a9253
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-18 13:34:03 +0800

    windows_t.h: add _t suffix to the typedef about Windows API.

--- include/windows_t.h
+++ include/windows_t.h
@@ -6,14 +6,17 @@
 /* 
 * Documents:
 * https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualalloc
-* https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
 * https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfree
+* https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
 * https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibrarya
 * https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary
 * https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress
 * https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-flushinstructioncache
 * https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createthread
 */
+
+#ifndef _WINDOWS_
+#define _WINDOWS_
 
 typedef byte*   LPCSTR;
 typedef uintptr HMODULE;
@@ -31,6 +34,8 @@ typedef uint    HANDLE;
 #define PAGE_EXECUTE_READ      0x20
 #define PAGE_EXECUTE_READWRITE 0x40
 #define PAGE_EXECUTE_WRITECOPY 0x80
+
+#endif // _WINDOWS_
 
 #define PE_FILE_HEADER_SIZE      24
 #define PE_OPT_HEADER_SIZE_64    240
@@ -59,42 +64,42 @@ typedef struct {
     uint32 FirstThunk;
 } PE_ImportDirectory;
 
-typedef uintptr (*VirtualAlloc)
+typedef uintptr (*VirtualAlloc_t)
 (
     uintptr lpAddress, uint dwSize, uint32 flAllocationType, uint32 flProtect
 );
 
-typedef bool (*VirtualFree)
+typedef bool (*VirtualFree_t)
 (
     uintptr lpAddress, uint dwSize, uint32 dwFreeType
 );
 
-typedef bool (*VirtualProtect)
+typedef bool (*VirtualProtect_t)
 (
     uintptr lpAddress, uint dwSize, uint32 flNewProtect, uint32* lpflOldProtect
 );
 
-typedef HMODULE (*LoadLibraryA)
+typedef HMODULE (*LoadLibraryA_t)
 (
     LPCSTR lpLibFileName
 );
 
-typedef bool (*FreeLibrary)
+typedef bool (*FreeLibrary_t)
 (
     HMODULE hLibModule
 );
 
-typedef uintptr (*GetProcAddress)
+typedef uintptr (*GetProcAddress_t)
 (
     HMODULE hModule, LPCSTR lpProcName
 );
 
-typedef bool (*FlushInstCache)
+typedef bool (*FlushInstructionCache_t)
 (
     HANDLE hProcess, uintptr lpBaseAddress, uint dwSize
 );
 
-typedef HANDLE (*CreateThread)
+typedef HANDLE (*CreateThread_t)
 (
     uintptr lpThreadAttributes, uint dwStackSize, uintptr lpStartAddress,
     uintptr lpParameter, uint32 dwCreationFlags, uint32* lpThreadId
