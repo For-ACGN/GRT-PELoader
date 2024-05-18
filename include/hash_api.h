25a77bd4dcc5953bdc114b897ee923653604170a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-18 13:31:48 +0800

    hash_api.h: add functions about HashAPI_A and HashAPI_W.

--- include/hash_api.h
+++ include/hash_api.h
@@ -3,11 +3,21 @@
 
 #include "go_types.h"
 
+typedef uintptr (*FindAPI_t)(uint hash, uint key);
+
 // FindAPI is used to FindAPI address by hash and key.
-#ifdef _WIN64
-uintptr FindAPI(uint64 hash, uint64 key);
-#elif _WIN32
-uintptr FindAPI(uint32 hash, uint32 key);
-#endif
+uintptr FindAPI(uint hash, uint key);
+
+// HashAPI_A is used to calculate Windows API hash by module
+// and function with key, module and function are ASCII.
+uint   HashAPI_A  (byte* module, byte* function, uint key);
+uint64 HashAPI64_A(byte* module, byte* function, uint64 key);
+uint32 HashAPI32_A(byte* module, byte* function, uint32 key);
+
+// HashAPI_W is used to calculate Windows API hash by module
+// and function with key, module is Unicode, function is ASCII.
+uint   HashAPI_W  (byte* module, byte* function, uint key);
+uint64 HashAPI64_W(byte* module, byte* function, uint64 key);
+uint32 HashAPI32_W(byte* module, byte* function, uint32 key);
 
 #endif // HASH_API_H
