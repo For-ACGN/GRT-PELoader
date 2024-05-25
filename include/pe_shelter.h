a3f0fb2417cb5257f86b884d083a31ce8c154a4b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-05-25 21:38:45 +0800

    pe_shelter.h: add FindAPI to PEShelter_Opts, replace to c_types.h.

--- include/pe_shelter.h
+++ include/pe_shelter.h
@@ -1,10 +1,14 @@
 #ifndef PE_SHELTER_H
 #define PE_SHELTER_H
 
-#include "go_types.h"
+#include "c_types.h"
 #include "windows_t.h"
+#include "hash_api.h"
 
 typedef struct {
+    // use custom FindAPI from Gleam-RT
+    FindAPI_t FindAPI;
+
     // use custom GetProcAddress for IAT hook
     GetProcAddress_t GetProcAddress;
 } PEShelter_Opts;
