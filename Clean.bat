d7d2d17f2b043c5d1b2f8c34fb48269784a3af66
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-04 13:46:43 +0800

    delete .vs\GRT-PELoader\v17\Solution.VC.db for fix cache bug.

--- Clean.bat
+++ Clean.bat
@@ -1,12 +1,13 @@
+rd  /S /Q ".vs\GRT-PELoader\v17\ipch"
 del /S /Q ".vs\GRT-PELoader\v17\Browse.VC.db"
-rd  /S /Q ".vs\GRT-PELoader\v17\ipch\AutoPCH"
+del /S /Q ".vs\GRT-PELoader\v17\Solution.VC.db"
 
 rd /S /Q "Debug"
 rd /S /Q "Release"
-rd /S /Q "x86"
 rd /S /Q "x64"
+rd /S /Q "x86"
 
-rd /S /Q "example\loader\x86"
-rd /S /Q "example\loader\x64"
 rd /S /Q "example\loader\Debug"
 rd /S /Q "example\loader\Release"
+rd /S /Q "example\loader\x64"
+rd /S /Q "example\loader\x86"
