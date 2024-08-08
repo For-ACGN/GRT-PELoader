a1e8e3a72617990a1d59b0c832700ae6f65cc39d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-08 16:02:16 +0800

    add test files about lib_string and pe_loader, reserve TestInitPELoader.

---
+++ test/lib_string_test.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include "c_types.h"
+#include "lib_string.h"
+#include "test.h"
+
+static bool TestStrlen_a();
+static bool TestStrlen_w();
+static bool TestStrcmp_a();
+static bool TestStrcmp_w();
+
+bool TestLibString()
+{
+    test_t tests[] = 
+    {
+        { TestStrlen_a },
+        { TestStrlen_w },
+        { TestStrcmp_a },
+        { TestStrcmp_w },
+    };
+    for (int i = 0; i < arrlen(tests); i++)
+    {
+        if (!tests[i]())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool TestStrlen_a()
+{
+    ascii str = "ascii";
+
+    if (strlen_a(str) != 5)
+    {
+        printf_s("strlen_a return incorrect length\n");
+        return false;
+    }
+    printf_s("test strlen_a passed\n");
+
+    str = "";
+    if (strlen_a(str) != 0)
+    {
+        printf_s("strlen_a return incorrect length\n");
+        return false;
+    }
+    printf_s("test strlen_a with null passed\n");
+    return true;
+}
+
+bool TestStrlen_w()
+{
+    utf16 str = L"unicode";
+
+    if (strlen_w(str) != 7)
+    {
+        printf_s("strlen_w return incorrect length\n");
+        return false;
+    }
+    printf_s("test strlen_w passed\n");
+
+    str = L"";
+    if (strlen_w(str) != 0)
+    {
+        printf_s("strlen_w return incorrect length\n");
+        return false;
+    }
+    printf_s("test strlen_w with null passed\n");
+    return true;
+}
+
+bool TestStrcmp_a()
+{
+    ascii s0 = "abc";
+    ascii s1 = "abc";
+    if (strcmp_a(s0, s1) != 0)
+    {
+        printf_s("strcmp_a return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_a with s0=s1 passed\n");
+
+    s0 = "acc";
+    s1 = "abc";
+    if (strcmp_a(s0, s1) != 1)
+    {
+        printf_s("strcmp_a return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_a with s0>s1 passed\n");
+
+    s0 = "aac";
+    s1 = "abc";
+    if (strcmp_a(s0, s1) != -1)
+    {
+        printf_s("strcmp_a return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_a with s0<s1 passed\n");
+    return true;
+}
+
+bool TestStrcmp_w()
+{
+    utf16 s0 = L"abc";
+    utf16 s1 = L"abc";
+    if (strcmp_w(s0, s1) != 0)
+    {
+        printf_s("strcmp_w return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_w with s0=s1 passed\n");
+
+    s0 = L"acc";
+    s1 = L"abc";
+    if (strcmp_w(s0, s1) != 1)
+    {
+        printf_s("strcmp_w return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_w with s0>s1 passed\n");
+
+    s0 = L"aac";
+    s1 = L"abc";
+    if (strcmp_w(s0, s1) != -1)
+    {
+        printf_s("strcmp_w return incorrect value\n");
+        return false;
+    }
+    printf_s("test strcmp_w with s0<s1 passed\n");
+    return true;
+}
---
+++ test/pe_loader_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "c_types.h"
+#include "errno.h"
+#include "pe_loader.h"
+#include "test.h"
+
+bool TestInitPELoader()
+{
+    return true;
+}
