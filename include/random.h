5362214a69d4add2a9d7e5833b552f1e8d88d0b4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-09 15:28:18 +0800

    random.h: add functions about generate random data for obfuscate.

---
+++ include/random.h
@@ -0,0 +1,39 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "c_types.h"
+
+// RandBuf is used to fill random bytes to the memory.
+void RandBuf(byte* buf, int64 size);
+
+// RandByte is used to generate random byte.
+byte RandByte(uint64 seed);
+
+// RandBool is used to generate random bool.
+bool RandBool(uint64 seed);
+
+// RandInt is used to generate random int.
+int RandInt(uint64 seed);
+
+// RandUint is used to generate random uint.
+uint RandUint(uint64 seed);
+
+// RandInt64 is used to generate random int64.
+int64 RandInt64(uint64 seed);
+
+// RandUint64 is used to generate random uint64.
+uint64 RandUint64(uint64 seed);
+
+// RandIntN is used to generate random int with range.
+int RandIntN(uint64 seed, int n);
+
+// RandUintN is used to generate random uint with range.
+uint RandUintN(uint64 seed, uint n);
+
+// RandInt64N is used to generate random int64 with range.
+int64 RandInt64N(uint64 seed, int64 n);
+
+// RandUint64N is used to generate random uint64 with range.
+uint64 RandUint64N(uint64 seed, uint64 n);
+
+#endif // RANDOM_H
