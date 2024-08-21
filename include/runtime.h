74a6c9129ccd42f6fff17ff2f87af14b63f0f726
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-21 14:39:16 +0800

    runtime.h: add more methods for generic shellcode development to Runtime_M.

--- include/runtime.h
+++ include/runtime.h
@@ -7,15 +7,31 @@
 #include "errno.h"
 
 // for generic shellcode development.
-typedef void   (*Sleep_t)(uint32 milliseconds);
-typedef void*  (*MemAlloc_t)(uint size);
-typedef void*  (*MemRealloc_t)(void* address, uint size);
-typedef bool   (*MemFree_t)(void* address);
+typedef void (*Sleep_t)(uint32 milliseconds);
+
+typedef void   (*RandBuf_t)(byte* buf, int64 size);
+typedef bool   (*RandBool_t)(uint64 seed);
+typedef int64  (*RandInt64_t)(uint64 seed);
+typedef uint64 (*RandUint64_t)(uint64 seed);
+typedef int64  (*RandInt64N_t)(uint64 seed, int64 n);
+typedef uint64 (*RandUint64N_t)(uint64 seed, uint64 n);
+
+typedef void (*EncryptBuf_t)(byte* buf, uint size, byte* key, byte* iv);
+typedef void (*DecryptBuf_t)(byte* buf, uint size, byte* key, byte* iv);
+
+typedef uint (*Compress_t)(void* dst, void* src);
+typedef uint (*Decompress_t)(void* dst, void* src);
+
+typedef void* (*MemAlloc_t)(uint size);
+typedef void* (*MemRealloc_t)(void* address, uint size);
+typedef bool  (*MemFree_t)(void* address);
+
 typedef HANDLE (*ThdNew_t)(void* address, void* parameter, bool track);
 typedef void   (*ThdExit_t)();
-typedef bool   (*GetArgument_t)(uint index, void** data, uint32* size);
-typedef bool   (*EraseArgument_t)(uint index);
-typedef void   (*EraseAllArgs_t)();
+
+typedef bool (*GetArgument_t)(uint index, void** data, uint32* size);
+typedef bool (*EraseArgument_t)(uint index);
+typedef void (*EraseAllArgs_t)();
 
 // GetProcAddress, GetProcAddressByName and GetProcAddressByHash
 // are use Hash API module for implement original GetProcAddress.
@@ -52,23 +68,52 @@ typedef struct {
 
 // Runtime_M contains exported runtime methods.
 typedef struct {
+    // misc module
     FindAPI_t FindAPI;
     Sleep_t   Sleep;
 
-    MemAlloc_t      MemAlloc;
-    MemRealloc_t    MemRealloc;
-    MemFree_t       MemFree;
-    ThdNew_t        NewThread;
-    ThdExit_t       ExitThread;
+    // random module
+    RandBuf_t     RandBuf;
+    RandBool_t    RandBool;
+    RandInt64_t   RandInt64;
+    RandUint64_t  RandUint64;
+    RandInt64N_t  RandInt64N;
+    RandUint64N_t RandUint64N;
+
+    // crypto module
+    EncryptBuf_t EncryptBuf;
+    DecryptBuf_t DecryptBuf;
+
+    // compress module
+    Compress_t   Compress;
+    Decompress_t Decompress;
+
+    // library tracker
+    LoadLibraryA_t   LoadLibraryA;
+    LoadLibraryW_t   LoadLibraryW;
+    FreeLibrary_t    FreeLibrary;
+    GetProcAddress_t GetProcAddress;
+
+    // memory tracker
+    MemAlloc_t   MemAlloc;
+    MemRealloc_t MemRealloc;
+    MemFree_t    MemFree;
+
+    // thread tracker
+    ThdNew_t  NewThread;
+    ThdExit_t ExitThread;
+
+    // argument store
     GetArgument_t   GetArgument;
     EraseArgument_t EraseArgument;
     EraseAllArgs_t  EraseAllArgs;
 
-    GetProcAddress_t         GetProcAddress;
+    // about IAT hooks
     GetProcAddressByName_t   GetProcAddressByName;
     GetProcAddressByHash_t   GetProcAddressByHash;
     GetProcAddressOriginal_t GetProcAddressOriginal;
     
+    // runtime core
     SleepHR_t SleepHR;
     Hide_t    Hide;
     Recover_t Recover;
