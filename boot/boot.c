850f1aa2fef5cd84fa844c6f5aad2207e8e078f7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-25 16:21:48 +0800

    boot.c: finish loadPELoaderConfig, improve Boot about initialize PE Loader.

--- boot/boot.c
+++ boot/boot.c
@@ -5,14 +5,15 @@
 #include "pe_loader.h"
 #include "boot.h"
 
-errno loadPELoaderConfig(Runtime_M* runtime, PELoader_Cfg* config);
+static errno loadPELoaderConfig(Runtime_M* runtime, PELoader_Cfg* config);
 
 errno Boot()
 {
+    // initialize Gleam-RT
     Runtime_Opts opts = {
         .BootInstAddress     = GetFuncAddr(&Boot),
         .NotEraseInstruction = false,
-        .NotAdjustProtect    = false,
+        .NotAdjustProtect    = true,
         .TrackCurrentThread  = false,
     };
     Runtime_M* runtime = InitRuntime(&opts);
@@ -21,7 +22,10 @@ errno Boot()
         return GetLastErrno();
     }
 
+    // load config and initialize PE Loader
     PELoader_Cfg cfg = {
+        .FindAPI = runtime->FindAPI,
+
         .Image       = NULL,
         .CommandLine = NULL,
         .StdInput    = NULL,
@@ -29,7 +33,6 @@ errno Boot()
         .StdError    = NULL,
         .WaitMain    = false,
 
-        .FindAPI       = runtime->FindAPI,
         .AdjustProtect = false,
     };
     PELoader_M* loader;
@@ -50,34 +53,77 @@ errno Boot()
         }
         break;
     }
-
+    runtime->EraseAllArgs();
     if (err != NO_ERROR)
     {
         runtime->Exit();
         return err;
     }
 
+    // execute PE file
+
+
+
 
     return NO_ERROR;
 }
 
-errno loadPELoaderConfig(Runtime_M* runtime, PELoader_Cfg* config)
+static errno loadPELoaderConfig(Runtime_M* runtime, PELoader_Cfg* config)
 {
     uint32 size;
-    byte* image = NULL;
-    if (!runtime->GetArgument(ARG_IDX_PE_IMAGE, &image, &size))
+    // load PE Image, it cannot be empty
+    if (!runtime->GetArgument(ARG_IDX_PE_IMAGE, &config->Image, &size))
     {
         return ERR_NOT_FOUND_PE_IMAGE;
     }
     if (size == 0)
     {
-        return ERR_EMPTY_PE_IMAGE;
+        return ERR_EMPTY_PE_IMAGE_DATA;
     }
-
-
-    //  
-        
-
-
+    // load command line, it can be empty
+    if (!runtime->GetArgument(ARG_IDX_COMMAND_LINE, &config->CommandLine, &size))
+    {
+        return ERR_NOT_FOUND_COMMAND_LINE;
+    }
+    if (size > 4096)
+    {
+        return ERR_COMMAND_LINE_TOO_LONG;
+    }
+    // load STD_INPUT_HANDLE, it can be zero
+    if (!runtime->GetArgument(ARG_IDX_STD_INPUT, &config->StdInput, &size))
+    {
+        return ERR_NOT_FOUND_STD_INPUT;
+    }
+    if (size != sizeof(HANDLE))
+    {
+        return ERR_INVALID_STD_INPUT;
+    }
+    // load STD_OUTPUT_HANDLE, it can be zero
+    if (!runtime->GetArgument(ARG_IDX_STD_OUTPUT, &config->StdOutput, &size))
+    {
+        return ERR_NOT_FOUND_STD_OUTPUT;
+    }
+    if (size != sizeof(HANDLE))
+    {
+        return ERR_INVALID_STD_OUTPUT;
+    }
+    // load STD_ERROR_HANDLE, it can be zero
+    if (!runtime->GetArgument(ARG_IDX_STD_ERROR, &config->StdError, &size))
+    {
+        return ERR_NOT_FOUND_STD_ERROR;
+    }
+    if (size != sizeof(HANDLE))
+    {
+        return ERR_INVALID_STD_ERROR;
+    }
+    // load wait main, it must be true of false
+    if (!runtime->GetArgument(ARG_IDX_WAIT_MAIN, &config->WaitMain, &size))
+    {
+        return ERR_NOT_FOUND_WAIT_MAIN;
+    }
+    if (size != sizeof(bool))
+    {
+        return ERR_INVALID_WAIT_MAIN;
+    }
     return NO_ERROR;
 }
