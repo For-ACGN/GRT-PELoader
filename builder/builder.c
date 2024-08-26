b8215a8f80d237555ac92f0fe82027864f3f8262
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-08-26 16:13:52 +0800

    builder.c: add a simple tool for extract shellcode, finish saveShellcode.

--- builder/builder.c
+++ builder/builder.c
@@ -1,10 +1,64 @@
 #include <stdio.h>
 #include "c_types.h"
+#include "errno.h"
 #include "runtime.h"
 #include "pe_loader.h"
+#include "boot.h"
 #include "epilogue.h"
+
+int saveShellcode();
+int testShellcode();
 
 int __cdecl main()
 {
+    int ret = saveShellcode();
+    if (ret != 0)
+    {
+        return ret;
+    }
+    ret = testShellcode();
+    if (ret != 0)
+    {
+        return ret;
+    }
+    printf_s("save shellcode successfully");
+    return 0;
+}
+
+int saveShellcode()
+{
+#ifdef _WIN64
+    FILE* file = fopen("../dist/PELoader_x64.bin", "wb");
+#elif _WIN32
+    FILE* file = fopen("../dist/PELoader_x86.bin", "wb");
+#endif
+    if (file == NULL)
+    {
+        printf_s("failed to create output file");
+        return 1;
+    }
+    uintptr begin = (uintptr)(&Boot);
+    uintptr end   = (uintptr)(&Epilogue);
+    uintptr size  = end - begin;
+    size_t  n = fwrite((byte*)begin, (size_t)size, 1, file);
+    if (n != 1)
+    {
+        printf_s("failed to save shellcode");
+        return 2;
+    }
+    fclose(file);
+    return 0;
+}
+
+int testShellcode()
+{
+    // TODO test
+    return 0;
+
+    errno errno = Boot();
+    if (errno != NO_ERROR)
+    {
+        return errno;
+    }
     return 0;
 }
