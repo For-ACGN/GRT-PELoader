cdf98c2328cd09885e3f171532c87c0249094b01
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-01 14:03:18 +0800

    runtime.asm: move code about define functions to this file.

--- asm/runtime.asm
+++ asm/runtime.asm
@@ -5,13 +5,19 @@ ENDIF
 .code
 
 IFDEF _WIN32
+  _InitRuntime@4 proc
   INCLUDE <runtime_x86.inst>
+  _InitRuntime@4 endp
 ELSE
+  InitRuntime proc
   INCLUDE <runtime_x64.inst>
+  InitRuntime endp
 ENDIF
 
 IFNDEF RELEASE_MODE
+  TestRuntimeArgStub proc
   INCLUDE <runtime_arg.inst>
+  TestRuntimeArgStub endp
 ENDIF
 
 end
