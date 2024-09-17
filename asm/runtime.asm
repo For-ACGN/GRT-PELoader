37433879cae878a00000040105e5974b9adba8e5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-17 14:08:13 +0800

    merge args_stub.asm to runtime.asm for fix instructions align, remove inst files.

--- asm/runtime.asm
+++ asm/runtime.asm
@@ -6,12 +6,22 @@ ENDIF
 
 IFDEF _WIN32
   _InitRuntime@4 proc
-  INCLUDE <runtime_x86.inst>
+  INCLUDE <inst/runtime_x86.inst>
   _InitRuntime@4 endp
 ELSE
   InitRuntime proc
-  INCLUDE <runtime_x64.inst>
+  INCLUDE <inst/runtime_x64.inst>
   InitRuntime endp
+ENDIF
+
+IFDEF _WIN32
+  _Argument_Stub@0 proc
+  INCLUDE <inst/argument_x86.inst>
+  _Argument_Stub@0 endp
+ELSE
+  Argument_Stub proc
+  INCLUDE <inst/argument_x64.inst>
+  Argument_Stub endp
 ENDIF
 
 end
