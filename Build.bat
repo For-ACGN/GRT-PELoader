88d5ffc23829145cadcb3f2e83287a91d6aaa1ef
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-20 15:13:21 +0800

    add a build script for extract PE Loader shellcode.

---
+++ Build.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo ========== initialize Visual Studio environment ==========
+if "%VisualStudio%" == "" (
+    echo environment variable "VisualStudio" is not set
+    exit /b 1
+)
+call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
+
+echo ================= clean builder old files ================
+rd /S /Q "builder\Release"
+rd /S /Q "builder\x64"
+rd /S /Q "Release"
+rd /S /Q "x64"
+
+echo ==================== generate builder ====================
+MSBuild.exe GRT-PELoader.sln /t:builder /p:Configuration=Release /p:Platform=x64
+MSBuild.exe GRT-PELoader.sln /t:builder /p:Configuration=Release /p:Platform=x86
+
+echo =============== extract PE Loader shellcode ==============
+del /S /Q dist
+cd builder
+echo --------extract shellcode for x64--------
+"..\x64\Release\builder.exe"
+echo --------extract shellcode for x86--------
+"..\Release\builder.exe"
+cd ..
+
+echo ==========================================================
+echo                  build shellcode finish!
+echo ==========================================================
