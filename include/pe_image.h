e601c37e79d2244951bd7dccd80233b09751a10c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-22 10:53:06 +0800

    pe_image.h: add macros about IMAGE_DIRECTORY_ENTRY, add Image_TLSDirectory.

--- include/pe_image.h
+++ include/pe_image.h
@@ -9,10 +9,25 @@
 #define PE_OPT_HEADER_SIZE_32    224
 #define PE_SECTION_HEADER_SIZE   40
 #define PE_DATA_DIRECTORY_SIZE   8
-#define PE_IMPORT_DIRECTORY_SIZE 20
 
 #define IMAGE_FILE_EXECUTABLE_IMAGE 0x0002
 #define IMAGE_FILE_DLL              0x2000
+
+#define	IMAGE_DIRECTORY_ENTRY_EXPORT         0
+#define	IMAGE_DIRECTORY_ENTRY_IMPORT         1
+#define	IMAGE_DIRECTORY_ENTRY_RESOURCE       2
+#define	IMAGE_DIRECTORY_ENTRY_EXCEPTION      3
+#define	IMAGE_DIRECTORY_ENTRY_SECURITY       4
+#define	IMAGE_DIRECTORY_ENTRY_BASERELOC      5
+#define	IMAGE_DIRECTORY_ENTRY_DEBUG          6
+#define	IMAGE_DIRECTORY_ENTRY_ARCHITECTURE   7
+#define	IMAGE_DIRECTORY_ENTRY_GLOBALPTR      8
+#define	IMAGE_DIRECTORY_ENTRY_TLS            9
+#define	IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG    10
+#define	IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT   11
+#define	IMAGE_DIRECTORY_ENTRY_IAT            12
+#define	IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT   13
+#define	IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR 14
 
 #define IMAGE_ORDINAL_FLAG64 0x8000000000000000
 #define IMAGE_ORDINAL_FLAG32 0x80000000
@@ -22,16 +37,25 @@
 #define IMAGE_REL_BASED_DIR64    10
 
 typedef struct {
-    DWORD VirtualAddress;
-    DWORD SizeOfBlock;
-} PE_ImageBaseRelocation;
-
-typedef struct {
     DWORD OriginalFirstThunk;
     DWORD TimeDateStamp;
     DWORD ForwarderChain;
     DWORD Name;
     DWORD FirstThunk;
-} PE_ImportDirectory;
+} Image_ImportDescriptor;
+
+typedef struct {
+    DWORD VirtualAddress;
+    DWORD SizeOfBlock;
+} Image_BaseRelocation;
+
+typedef struct {
+    UINT   StartAddressOfRawData;
+    UINT   EndAddressOfRawData;
+    UINT*  AddressOfIndex;
+    LPVOID AddressOfCallBacks;
+    DWORD  SizeOfZeroFill;
+    DWORD  Characteristics;
+} Image_TLSDirectory;
 
 #endif // PE_IMAGE_H
