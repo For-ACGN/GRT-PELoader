f79b5d3009a4a22e071e7b645ee04c54807e0628
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-09-22 11:59:25 +0800

    hash_api.bat: add VirtualLock, VirtualUnlock and ExitThread.

--- script/hash_api.bat
+++ script/hash_api.bat
@@ -11,7 +11,10 @@ hash_api -fmt 64 -conc -func GetProcAddress
 hash_api -fmt 64 -conc -func VirtualAlloc
 hash_api -fmt 64 -conc -func VirtualFree
 hash_api -fmt 64 -conc -func VirtualProtect
+hash_api -fmt 64 -conc -func VirtualLock
+hash_api -fmt 64 -conc -func VirtualUnlock
 hash_api -fmt 64 -conc -func CreateThread
+hash_api -fmt 64 -conc -func ExitThread
 hash_api -fmt 64 -conc -func FlushInstructionCache
 hash_api -fmt 64 -conc -func CreateMutexA
 hash_api -fmt 64 -conc -func ReleaseMutex
@@ -33,7 +36,10 @@ hash_api -fmt 32 -conc -func GetProcAddress
 hash_api -fmt 32 -conc -func VirtualAlloc
 hash_api -fmt 32 -conc -func VirtualFree
 hash_api -fmt 32 -conc -func VirtualProtect
+hash_api -fmt 32 -conc -func VirtualLock
+hash_api -fmt 32 -conc -func VirtualUnlock
 hash_api -fmt 32 -conc -func CreateThread
+hash_api -fmt 32 -conc -func ExitThread
 hash_api -fmt 32 -conc -func FlushInstructionCache
 hash_api -fmt 32 -conc -func CreateMutexA
 hash_api -fmt 32 -conc -func ReleaseMutex
