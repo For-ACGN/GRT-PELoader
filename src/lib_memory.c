7233c543c22a0a1e0c44c4410f980e4c356abbf3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-02 18:51:56 +0800

    lib_memory.c: improve variable name, add mem_cmp, rename mem_clean to mem_init.

--- src/lib_memory.c
+++ src/lib_memory.c
@@ -9,44 +9,6 @@
 // shellcode to be incorrect.
 #pragma optimize("", off)
 
-bool mem_equal(void* dst, void* src, uint size)
-{
-    if (size == 0)
-    {
-        return true;
-    }
-    byte* d = (byte*)dst;
-    byte* s = (byte*)src;
-    for (uint i = 0; i < size; i++)
-    {
-        if (*d != *s)
-        {
-            return false;
-        }
-        d++;
-        s++;
-    }
-    return true;
-}
-
-bool mem_zero(void* dst, uint size)
-{
-    if (size == 0)
-    {
-        return true;
-    }
-    byte* d = (byte*)dst;
-    for (uint i = 0; i < size; i++)
-    {
-        if (*d != 0)
-        {
-            return false;
-        }
-        d++;
-    }
-    return true;
-}
-
 void mem_copy(void* dst, void* src, uint size)
 {
     if (size == 0)
@@ -63,27 +25,91 @@ void mem_copy(void* dst, void* src, uint size)
     }
 }
 
-void mem_set(void* ptr, byte val, uint num)
-{
-    if (num == 0)
-    {
-        return;
-    }
-    byte* addr = (byte*)ptr;
-    for (uint i = 0; i < num; i++)
-    {
-        *addr = val;
-        addr++;
-    }
-}
-
-void mem_clean(void* ptr, uint num)
+void mem_init(void* ptr, uint num)
 {
     if (num == 0)
     {
         return;
     }
     mem_set(ptr, 0, num);
+}
+
+void mem_set(void* ptr, byte val, uint num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    byte* p = (byte*)ptr;
+    for (uint i = 0; i < num; i++)
+    {
+        *p = val;
+        p++;
+    }
+}
+
+int mem_cmp(void* a, void* b, uint size)
+{
+    if (size == 0)
+    {
+        return 0;
+    }
+    byte* p0 = (byte*)a;
+    byte* p1 = (byte*)b;
+    for (uint i = 0; i < size; i++)
+    {
+        if (*p0 == *p1)
+        {
+            p0++;
+            p1++;
+            continue;
+        }
+        if (*p0 > *p1)
+        {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+bool mem_equal(void* a, void* b, uint size)
+{
+    if (size == 0)
+    {
+        return true;
+    }
+    byte* p0 = (byte*)a;
+    byte* p1 = (byte*)b;
+    for (uint i = 0; i < size; i++)
+    {
+        if (*p0 != *p1)
+        {
+            return false;
+        }
+        p0++;
+        p1++;
+    }
+    return true;
+}
+
+bool mem_is_zero(void* ptr, uint size)
+{
+    if (size == 0)
+    {
+        return true;
+    }
+    byte* p = (byte*)ptr;
+    for (uint i = 0; i < size; i++)
+    {
+        if (*p != 0)
+        {
+            return false;
+        }
+        p++;
+    }
+    return true;
 }
 
 #pragma optimize("", on)
