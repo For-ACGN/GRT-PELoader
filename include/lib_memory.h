4c77e08019646931a826ff100d82416e763f3977
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-02 18:50:41 +0800

    lib_memory.h: add mem_cmp, rename mem_clean to mem_init.

--- include/lib_memory.h
+++ include/lib_memory.h
@@ -4,22 +4,29 @@
 #include "c_types.h"
 
 typedef void* (*malloc_t)(uint size);
-typedef void* (*realloc_t)(void* address, uint size);
-typedef bool  (*free_t)(void* address);
-
-// mem_equal is used to compare the memory is equal.
-bool mem_equal(void* dst, void* src, uint size);
-
-// mem_zero is used to check the destination memory are all zero.
-bool mem_zero(void* dst, uint size);
+typedef void* (*calloc_t)(uint num, uint size);
+typedef void* (*realloc_t)(void* ptr, uint size);
+typedef bool  (*free_t)(void* ptr);
 
 // mem_copy is used to copy source memory data to the destination.
 void mem_copy(void* dst, void* src, uint size);
 
+// mem_init is used to fill the memory with zero.
+void mem_init(void* ptr, uint num);
+
 // mem_set is used to fill the memory with value.
 void mem_set(void* ptr, byte val, uint num);
 
-// mem_clean is used to fill the memory with zero.
-void mem_clean(void* ptr, uint num);
+// mem_cmp is used to compare memory data with size.
+// if a = b, return 0
+// if a > b, return 1
+// if a < b, return -1
+int mem_cmp(void* a, void* b, uint size);
+
+// mem_equal is used to compare the memory is equal.
+bool mem_equal(void* a, void* b, uint size);
+
+// mem_zero is used to check the memory are all zero.
+bool mem_is_zero(void* ptr, uint size);
 
 #endif // LIB_MEMORY_H
