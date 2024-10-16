6470770d452b426608e5cc73bcb2ec5616ec2512
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-16 14:07:05 +0800

    build.h: add NO_RUNTIME for not include Gleam-RT for test and debug.

--- test/build.h
+++ test/build.h
@@ -2,7 +2,15 @@
 #define BUILD_H
 
 // RELEASE_MODE: remove debug modules for generate shellcode
+// NO_RUNTIME:   not include Gleam-RT for test and debug
 
 // #define RELEASE_MODE
+// #define NO_RUNTIME
+
+// disable special warnings for NO_RUNTIME
+#ifdef NO_RUNTIME
+    #pragma warning(disable: 4100)
+    #pragma warning(disable: 4189)
+#endif
 
 #endif // BUILD_H
