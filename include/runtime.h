9e6c49035f5cfc382f267fcf78a2198ca05555d4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-16 19:38:56 +0800

    runtime.h: update runtime about WinFile and WinHTTP modules.

--- include/runtime.h
+++ include/runtime.h
@@ -3,6 +3,7 @@
 
 #include "c_types.h"
 #include "windows_t.h"
+#include "lib_string.h"
 #include "hash_api.h"
 #include "errno.h"
 
@@ -23,7 +24,6 @@ typedef void  (*MemFree_t)(void* ptr);
 // about thread module
 typedef HANDLE (*ThdNew_t)(void* address, void* parameter, bool track);
 typedef void   (*ThdExit_t)();
-typedef void   (*Sleep_t)(uint32 milliseconds);
 
 // about argument store
 typedef bool (*GetArgValue_t)(uint index, void* value, uint32* size);
@@ -31,17 +31,38 @@ typedef bool (*GetArgPointer_t)(uint index, void** pointer, uint32* size);
 typedef bool (*EraseArgument_t)(uint index);
 typedef void (*EraseAllArgs_t)();
 
-// GetProcAddress, GetProcAddressByName and GetProcAddressByHash
-// are use Hash API module for implement original GetProcAddress.
-// GetProcAddressOriginal is not recommend, usually use
-// GetProcAddressByName with hook FALSE instead it.
-// These methods are used for IAT hooks or common shellcode.
-typedef void* (*GetProcAddressByName_t)(HMODULE hModule, LPCSTR lpProcName, bool hook);
-typedef void* (*GetProcAddressByHash_t)(uint hash, uint key, bool hook);
-typedef void* (*GetProcAddressOriginal_t)(HMODULE hModule, LPCSTR lpProcName);
+// about Win File
+// The buffer allocated from ReadFile must call Runtime_M.Memory.Free().
+typedef errno (*ReadFileA_t)(LPSTR path, byte** buf, int64* size);
+typedef errno (*ReadFileW_t)(LPWSTR path, byte** buf, int64* size);
+typedef errno (*WriteFileA_t)(LPSTR path, byte* buf, int64 size);
+typedef errno (*WriteFileW_t)(LPWSTR path, byte* buf, int64 size);
+
+// about WinHTTP
+// The BodyBuf allocated must call Runtime_M.Memory.Free().
+#ifndef WIN_HTTP_H
+typedef struct {
+    UTF16  UserAgent;   // default User-Agent
+    UTF16  ContentType; // for POST method
+    UTF16  Headers;     // split by "\r\n"
+    UTF16  Proxy;       // http://user:pass@host.com/
+    uint32 Timeout;     // millseconds
+    uint8  AccessType;  // reference document about WinHttpOpen
+} WinHTTP_Opts;
+
+typedef struct {
+    int32 StatusCode;
+    UTF16 Headers;
+    void* BodyBuf;
+    uint  BodySize;
+} WinHTTP_Resp;
+#endif // WIN_HTTP_H
+
+typedef errno (*Get_t)(UTF16 url, WinHTTP_Opts* opts, WinHTTP_Resp* resp);
+typedef errno (*Post_t)(UTF16 url, void* body, WinHTTP_Opts* opts, WinHTTP_Resp* resp);
 
 // about random module
-typedef void   (*RandBuf_t)(byte* buf, int64 size);
+typedef void   (*RandBuffer_t)(byte* buf, int64 size);
 typedef bool   (*RandBool_t)(uint64 seed);
 typedef int64  (*RandInt64_t)(uint64 seed);
 typedef uint64 (*RandUint64_t)(uint64 seed);
@@ -55,6 +76,15 @@ typedef void (*Decrypt_t)(byte* buf, uint size, byte* key, byte* iv);
 // about compress module
 typedef uint (*Compress_t)(void* dst, void* src);
 typedef uint (*Decompress_t)(void* dst, void* src);
+
+// GetProcAddress, GetProcAddressByName and GetProcAddressByHash
+// are use Hash API module for implement original GetProcAddress.
+// GetProcAddressOriginal is not recommend, usually use
+// GetProcAddressByName with hook FALSE instead it.
+// These methods are used for IAT hooks or common shellcode.
+typedef void* (*GetProcByName_t)(HMODULE hModule, LPCSTR lpProcName, bool hook);
+typedef void* (*GetProcByHash_t)(uint hash, uint key, bool hook);
+typedef void* (*GetProcOriginal_t)(HMODULE hModule, LPCSTR lpProcName);
 
 // runtime core methods
 // it is NOT recommended use "Hide" and "Recover", these functions
@@ -82,62 +112,84 @@ typedef struct {
 
 // Runtime_M contains exported runtime methods.
 typedef struct {
-    // about hash api
-    FindAPI_t   FindAPI;
-    FindAPI_A_t FindAPI_A;
-    FindAPI_W_t FindAPI_W;
+    struct {
+        FindAPI_t   FindAPI;
+        FindAPI_A_t FindAPI_A;
+        FindAPI_W_t FindAPI_W;
+    } HashAPI;
 
-    // library tracker
-    LoadLibraryA_t   LoadLibraryA;
-    LoadLibraryW_t   LoadLibraryW;
-    LoadLibraryExA_t LoadLibraryExA;
-    LoadLibraryExW_t LoadLibraryExW;
-    FreeLibrary_t    FreeLibrary;
-    GetProcAddress_t GetProcAddress;
+    struct {
+        LoadLibraryA_t   LoadA;
+        LoadLibraryW_t   LoadW;
+        LoadLibraryExA_t LoadExA;
+        LoadLibraryExW_t LoadExW;
+        FreeLibrary_t    Free;
+        GetProcAddress_t GetProc;
+    } Library;
 
-    // memory tracker
-    MemAlloc_t   MemAlloc;
-    MemCalloc_t  MemCalloc;
-    MemRealloc_t MemRealloc;
-    MemFree_t    MemFree;
+    struct {
+        MemAlloc_t   Alloc;
+        MemCalloc_t  Calloc;
+        MemRealloc_t Realloc;
+        MemFree_t    Free;
+    } Memory;
 
-    // thread tracker
-    ThdNew_t  NewThread;
-    ThdExit_t ExitThread;
-    Sleep_t   Sleep;
+    struct {
+        ThdNew_t  New;
+        ThdExit_t Exit;
+        Sleep_t   Sleep;
+    } Thread;
 
-    // argument store
-    GetArgValue_t   GetArgValue;
-    GetArgPointer_t GetArgPointer;
-    EraseArgument_t EraseArgument;
-    EraseAllArgs_t  EraseAllArgs;
+    struct {
+        GetArgValue_t   GetValue;
+        GetArgPointer_t GetPointer;
+        EraseArgument_t Erase;
+        EraseAllArgs_t  EraseAll;
+    } Argument;
 
-    // about IAT hooks
-    GetProcAddressByName_t   GetProcAddressByName;
-    GetProcAddressByHash_t   GetProcAddressByHash;
-    GetProcAddressOriginal_t GetProcAddressOriginal;
+    struct {
+        ReadFileA_t  ReadFileA;
+        ReadFileW_t  ReadFileW;
+        WriteFileA_t WriteFileA;
+        WriteFileW_t WriteFileW;
+    } WinFile;
     
-    // random module
-    RandBuf_t     RandBuf;
-    RandBool_t    RandBool;
-    RandInt64_t   RandInt64;
-    RandUint64_t  RandUint64;
-    RandInt64N_t  RandInt64N;
-    RandUint64N_t RandUint64N;
+    struct {
+        Get_t  Get;
+        Post_t Post;
+    } WinHTTP;
 
-    // crypto module
-    Encrypt_t Encrypt;
-    Decrypt_t Decrypt;
+    struct {
+        RandBuffer_t  Buffer;
+        RandBool_t    Bool;
+        RandInt64_t   Int64;
+        RandUint64_t  Uint64;
+        RandInt64N_t  Int64N;
+        RandUint64N_t Uint64N;
+    } Random;
 
-    // compress module
-    Compress_t   Compress;
-    Decompress_t Decompress;
+    struct {
+        Encrypt_t Encrypt;
+        Decrypt_t Decrypt;
+    } Crypto;
 
-    // runtime core methods
-    SleepHR_t SleepHR;
-    Hide_t    Hide;
-    Recover_t Recover;
-    Exit_t    Exit;
+    struct {
+        Compress_t   Compress;
+        Decompress_t Decompress;
+    } Compressor;
+
+    struct {
+        GetProcByName_t   GetProcByName;
+        GetProcByHash_t   GetProcByHash;
+        GetProcOriginal_t GetProcOriginal;
+    } IAT;
+
+    struct {
+        SleepHR_t Sleep;
+        Hide_t    Hide;
+        Recover_t Recover;
+        Exit_t    Exit;
+    } Core;
 } Runtime_M;
 
 // InitRuntime is used to initialize runtime and return module methods.
