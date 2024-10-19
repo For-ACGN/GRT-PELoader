a5838d9ed81e7a44a1e17819842825fe67d7a2e2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-19 16:02:32 +0800

    build.h: add SHELLCODE_MODE for run unit tests under PE Loader shellcode.

--- test/build.h
+++ test/build.h
@@ -1,11 +1,17 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-// RELEASE_MODE: remove debug modules for generate shellcode
-// NO_RUNTIME:   not include Gleam-RT for test and debug
+// RELEASE_MODE:   remove debug modules for generate shellcode
+// SHELLCODE_MODE: run unit tests under PE Loader shellcode
+// NO_RUNTIME:     not include Gleam-RT for test and debug
 
 // #define RELEASE_MODE
+// #define SHELLCODE_MODE
 // #define NO_RUNTIME
+
+#ifdef SHELLCODE_MODE
+    #define RELEASE_MODE
+#endif // SHELLCODE_MODE
 
 // disable special warnings for NO_RUNTIME
 #ifdef NO_RUNTIME
