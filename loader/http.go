43cbf69e72468ef91bf3324674af95d676e5f29d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-28 19:37:03 +0800

    add HTTP mode, finish the basic mode with URL, add HTTPOptions.

--- loader/http.go
+++ loader/http.go
@@ -1,1 +1,50 @@
 package loader
+
+import (
+	"bytes"
+	"time"
+)
+
+// +-----------+-----+
+// | mode flag | URL |
+// +-----------+-----+
+// |   byte    | var |
+// +-----------+-----+
+
+const modeHTTP = 3
+
+// HTTP is the HTTP mode.
+type HTTP struct {
+	URL  string
+	Opts *HTTPOptions
+}
+
+// HTTPOptions contain HTTP mode options.
+type HTTPOptions struct {
+	ProxyURL string
+	Timeout  time.Duration
+}
+
+// NewHTTP is used to create image config with HTTP mode.
+func NewHTTP(url string, opts *HTTPOptions) Image {
+	return &HTTP{
+		URL:  url,
+		Opts: opts,
+	}
+}
+
+// Encode implement Image interface.
+func (f *HTTP) Encode() ([]byte, error) {
+	config := bytes.NewBuffer(make([]byte, 0, 64))
+	// write the mode
+	config.WriteByte(modeHTTP)
+	// write the URL
+	config.WriteString(stringToUTF16(f.URL + "\x00"))
+	// TODO write the options
+	return config.Bytes(), nil
+}
+
+// Mode implement Image interface.
+func (f *HTTP) Mode() string {
+	return ModeHTTP
+}
