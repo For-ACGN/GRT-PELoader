bd0a9ccffae9e5de39ff0dded86fb350eb03f5c3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-28 18:27:16 +0800

    finish embed mode, use github.com/bovarysme/lzss for compress image.

---
+++ loader/embed.go
@@ -0,0 +1,68 @@
+package loader
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/bovarysme/lzss"
+)
+
+const modeEmbed = 1
+
+const (
+	disableCompress = 0
+	enableCompress  = 1
+)
+
+// Embed is the embed mode.
+type Embed struct {
+	Image    []byte
+	Compress bool
+}
+
+// NewEmbed is used to create image config with embed mode.
+func NewEmbed(image []byte, compress bool) *Embed {
+	return &Embed{
+		Image:    image,
+		Compress: compress,
+	}
+}
+
+// Encode implement Image interface.
+func (e *Embed) Encode() ([]byte, error) {
+	config := bytes.NewBuffer(make([]byte, 0, 16))
+	// write the mode
+	config.WriteByte(modeEmbed)
+	if !e.Compress {
+		config.WriteByte(disableCompress)
+		config.Write(e.Image)
+		return config.Bytes(), nil
+	}
+	// set the compressed flag
+	config.WriteByte(enableCompress)
+	// compress PE image
+	buf := bytes.NewBuffer(make([]byte, 0, len(e.Image)/2))
+	w := lzss.NewWriter(buf)
+	_, err := w.Write(e.Image)
+	if err != nil {
+		return nil, err
+	}
+	err = w.Close()
+	if err != nil {
+		return nil, err
+	}
+	// write raw size
+	size := binary.LittleEndian.AppendUint32(nil, uint32(len(e.Image)))
+	config.Write(size)
+	// write compressed size
+	size = binary.LittleEndian.AppendUint32(nil, uint32(buf.Len()))
+	config.Write(size)
+	// write compressed PE image
+	config.Write(buf.Bytes())
+	return config.Bytes(), nil
+}
+
+// Mode implement Image interface.
+func (e *Embed) Mode() string {
+	return ModeEmbed
+}
