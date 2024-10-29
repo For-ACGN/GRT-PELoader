bec18490c6e20d2b55d07ad6c37b2d2e43b21b34
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-29 15:13:47 +0800

    refactor the generator command line program, use the loader package.

--- tool/generator/main.go
+++ tool/generator/main.go
@@ -3,27 +3,24 @@ package main
 import (
 	"bytes"
 	"debug/pe"
-	"encoding/binary"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-	"unicode/utf16"
 
-	"github.com/RSSU-Shellcode/GRT-Config/argument"
 	"github.com/RSSU-Shellcode/GRT-Config/option"
+	"github.com/RSSU-Shellcode/GRT-PELoader/loader"
 )
 
 var (
-	tplDir  string
-	mode    string
-	arch    int
-	pePath  string
-	cmdLine string
-	wait    bool
-	outPath string
-	options option.Options
+	tplDir   string
+	mode     string
+	arch     int
+	pePath   string
+	options  loader.Options
+	compress bool
+	httpOpts loader.HTTPOptions
+	outPath  string
 )
 
 func init() {
@@ -31,30 +28,32 @@ func init() {
 	flag.StringVar(&mode, "mode", "", "select load mode")
 	flag.IntVar(&arch, "arch", 0, "set shellcode template architecture")
 	flag.StringVar(&pePath, "pe", "", "set input PE file path")
-	flag.StringVar(&cmdLine, "cmd", "", "set command line for exe")
-	flag.BoolVar(&wait, "wait", false, "wait for shellcode to exit")
+	flag.StringVar(&options.ImageName, "im", "", "set the image name about command line")
+	flag.StringVar(&options.CommandLine, "cmd", "", "set command line for exe")
+	flag.BoolVar(&options.WaitMain, "wait", false, "wait for shellcode to exit")
+	flag.BoolVar(&compress, "compress", true, "compress image when use embed mode")
 	flag.StringVar(&outPath, "o", "output.bin", "set output file path")
-	option.Flag(&options)
+	option.Flag(&options.Runtime)
 	flag.Parse()
 }
 
 func main() {
+	if pePath == "" {
+		flag.Usage()
+		return
+	}
+
 	fmt.Println("load PE Loader templates")
 	ldrX64, err := os.ReadFile(filepath.Join(tplDir, "PELoader_x64.bin"))
 	checkError(err)
 	ldrX86, err := os.ReadFile(filepath.Join(tplDir, "PELoader_x86.bin"))
 	checkError(err)
 
-	peConfig := make([]byte, 1)
+	// create image config
+	var image loader.Image
 	switch mode {
 	case "embed":
-		peConfig[0] = 1
 		fmt.Println("use embed image mode")
-
-		// disable compression
-		// TODO update it
-		peConfig = append(peConfig, 0)
-
 		fmt.Println("parse PE image file")
 		peData, err := os.ReadFile(pePath)
 		checkError(err)
@@ -63,99 +62,56 @@ func main() {
 		switch peFile.OptionalHeader.(type) {
 		case *pe.OptionalHeader64:
 			arch = 64
+			fmt.Println("image architecture: x64")
 		case *pe.OptionalHeader32:
 			arch = 32
+			fmt.Println("image architecture: x86")
 		default:
 			fmt.Println("unknown optional header type")
 			return
 		}
-		// write length
-		buf := make([]byte, 4)
-		binary.LittleEndian.PutUint32(buf, uint32(len(peData)))
-		peConfig = append(peConfig, buf...)
-		// write pe image
-		peConfig = append(peConfig, peData...)
+		if compress {
+			fmt.Println("enable PE image compression")
+			s := (len(peData) / (1024 * 1024)) + 1
+			fmt.Printf("please wait for about %d seconds for compress\n", s)
+		}
+		image = loader.NewEmbed(peData, compress)
 	case "file":
-		peConfig[0] = 2
-		path := stringToUTF16(pePath + "\x00")
-		peConfig = append(peConfig, path...)
+		fmt.Println("use local file mode")
+		image = loader.NewFile(pePath)
 	case "http":
-		peConfig[0] = 3
-		path := stringToUTF16(pePath + "\x00")
-		peConfig = append(peConfig, path...)
+		fmt.Println("use http mode")
+		image = loader.NewHTTP(pePath, &httpOpts)
 	default:
 		fmt.Println("unknown load mode")
 		return
 	}
 
-	var (
-		template  []byte
-		stdHandle []byte
-	)
+	// select shellcode template
+	var template []byte
 	switch arch {
-	case 64:
-		template = ldrX64
-		stdHandle = make([]byte, 8)
-		fmt.Println("select template for x64")
 	case 32:
 		template = ldrX86
-		stdHandle = make([]byte, 4)
 		fmt.Println("select template for x86")
+	case 64:
+		template = ldrX64
+		fmt.Println("select template for x64")
 	default:
 		fmt.Println("unknown template architecture")
 		return
 	}
 
-	fmt.Println("set runtime options to template")
-	output, err := option.Set(template, &options)
+	fmt.Println("generate GRT-PELoader from template")
+	inst, err := loader.CreateInstance(template, arch, image, &options)
 	checkError(err)
 
-	fmt.Println("encode arguments to stub")
-	var (
-		cmdLineA []byte
-		cmdLineW []byte
-	)
-	if cmdLine != "" {
-		peName := filepath.Base(pePath)
-		if peName == string(filepath.Separator) {
-			peName = "test.exe"
-		}
-		if strings.Contains(peName, " ") {
-			peName = "\"" + peName + "\""
-		}
-		peName += " "
-		cmdLine = peName + cmdLine + "\x00"
-		cmdLineA = []byte(cmdLine)
-		cmdLineW = []byte(stringToUTF16(cmdLine))
-	}
-
-	argWait := make([]byte, 1)
-	if wait {
-		argWait[0] = 1
-	}
-	args := [][]byte{
-		peConfig, cmdLineA, cmdLineW,
-		stdHandle, stdHandle, stdHandle,
-		argWait,
-	}
-	stub, err := argument.Encode(args...)
+	outPath, err = filepath.Abs(outPath)
 	checkError(err)
-
-	fmt.Println("generate shellcode about PE Loader")
-	output = append(output, stub...)
-	err = os.WriteFile(outPath, output, 0600)
+	fmt.Println("save instance to:", outPath)
+	err = os.WriteFile(outPath, inst, 0600)
 	checkError(err)
 
 	fmt.Println("generate shellcode successfully")
-}
-
-func stringToUTF16(s string) string {
-	w := utf16.Encode([]rune(s))
-	output := make([]byte, len(w)*2)
-	for i := 0; i < len(w); i++ {
-		binary.LittleEndian.PutUint16(output[i*2:], w[i])
-	}
-	return string(output)
 }
 
 func checkError(err error) {
