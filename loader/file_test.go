dc3752ad8cfd157149b91181ba5626d72382e85c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-10-29 13:21:28 +0800

    add TestFile, finish sub tests about common and mode.

--- loader/file_test.go
+++ loader/file_test.go
@@ -1,1 +1,29 @@
 package loader
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFile(t *testing.T) {
+	image := make([]byte, 512)
+	_, err := rand.Read(image[:256])
+	require.NoError(t, err)
+
+	t.Run("common", func(t *testing.T) {
+		file := NewFile("C:\\Windows\\System32\\cmd.exe")
+
+		config, err := file.Encode()
+		require.NoError(t, err)
+
+		fmt.Println(config)
+	})
+
+	t.Run("mode", func(t *testing.T) {
+		file := NewFile("C:\\Windows\\System32\\cmd.exe")
+		require.Equal(t, ModeFile, file.Mode())
+	})
+}
