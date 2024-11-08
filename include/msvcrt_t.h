2376c0deae513872e45306c8c7539081f9fa89b4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-08 13:42:38 +0800

    msvcrt_t.h: add __cdecl for fix bug, add msvcrt prefix.

--- include/msvcrt_t.h
+++ include/msvcrt_t.h
@@ -3,12 +3,12 @@
 
 #include "c_types.h"
 
-typedef int (*getmainargs_t)
+typedef int (__cdecl *msvcrt_getmainargs_t)
 (
     int* argc, byte*** argv, byte*** env, int doWildCard, void* startInfo
 );
 
-typedef int (*wgetmainargs_t)
+typedef int (__cdecl *msvcrt_wgetmainargs_t)
 (
     int* argc, uint16*** argv, uint16*** env, int doWildCard, void* startInfo
 );
